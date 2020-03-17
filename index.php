931e44401d48d32306e43b127a251786ed91347e
Rafael Barbaglia <rafael.barbaglia@gmail.com>
2020-03-17 20:47:38 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+
+//print_r($categorias);
+
+$nome = 'Rafael';
+$idade = 34;
+
+var_dump($nome, $idade);
