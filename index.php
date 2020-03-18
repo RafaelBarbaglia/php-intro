b3941fc6b23abf621b39d9f6db2189782dc84b85
Rafael Barbaglia <45952236+RafaelBarbaglia@users.noreply.github.com>
2020-03-18 01:02:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Formulário de inscrição</title>
     <meta name="author" content="">
     <meta name="description" content="">
-    <meta name="viewport" content="width-device-width, initial-scale='">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
 </head>
 <body>
 
